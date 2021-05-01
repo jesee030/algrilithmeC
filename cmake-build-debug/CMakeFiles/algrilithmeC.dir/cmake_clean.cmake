34924e9ca2b7fe8f6d60d8050e78bbdc2885f859
keyterrr <desiasd@qq.com>
2021-05-01 15:12:00 +0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/algrilithmeC.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/algrilithmeC.dir/main.c.obj"
+  "algrilithmeC.exe"
+  "algrilithmeC.exe.manifest"
+  "algrilithmeC.pdb"
+  "libalgrilithmeC.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/algrilithmeC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
